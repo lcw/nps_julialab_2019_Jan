a2a67b1bbac7fe7166fd86bd2e820fb2f49fbd3d
Lucas C Wilcox <lucas@swirlee.com>
2019-03-14 11:51:50 -0500

    Add attempt to do a pure CUDAnative kernel

--- vanilladeriv.jl
+++ vanilladeriv.jl
@@ -760,6 +760,151 @@ function volumerhs_v4!(::DEV,
 end
 # }}}
 
+# {{{ volume_v5
+function volumerhs_v5!(::Val{3},
+                       ::Val{N},
+                       ::Val{nmoist},
+                       ::Val{ntrace},
+                       rhs,
+                       Q,
+                       vgeo,
+                       gravity,
+                       D,
+                       nelem) where {N, nmoist, ntrace}
+
+  nvar = _nstate + nmoist + ntrace
+
+  Nq = N + 1
+
+  s_D = @cuStaticSharedMem eltype(D) (Nq, Nq)
+  s_F = @cuStaticSharedMem eltype(Q) (Nq, Nq, _nstate)
+  s_G = @cuStaticSharedMem eltype(Q) (Nq, Nq, _nstate)
+  s_H = @cuStaticSharedMem eltype(Q) (Nq, Nq, _nstate)
+
+  r_rhsρ = MArray{Tuple{Nq},eltype(rhs)}(undef)
+  r_rhsU = MArray{Tuple{Nq},eltype(rhs)}(undef)
+  r_rhsV = MArray{Tuple{Nq},eltype(rhs)}(undef)
+  r_rhsW = MArray{Tuple{Nq},eltype(rhs)}(undef)
+  r_rhsE = MArray{Tuple{Nq},eltype(rhs)}(undef)
+
+  e = blockIdx().x
+  j = threadIdx().y
+  i = threadIdx().x
+
+  @inbounds begin
+  for k in 1:Nq
+    r_rhsρ[k] = zero(eltype(rhs))
+    r_rhsU[k] = zero(eltype(rhs))
+    r_rhsV[k] = zero(eltype(rhs))
+    r_rhsW[k] = zero(eltype(rhs))
+    r_rhsE[k] = zero(eltype(rhs))
+  end
+
+  # fetch D into shared
+  s_D[i, j] = D[i, j]
+
+  for k in 1:Nq
+    sync_threads()
+
+    # Load values will need into registers
+    MJ = vgeo[i, j, k, _MJ, e]
+    ξx, ξy, ξz = vgeo[i,j,k,_ξx,e], vgeo[i,j,k,_ξy,e], vgeo[i,j,k,_ξz,e]
+    ηx, ηy, ηz = vgeo[i,j,k,_ηx,e], vgeo[i,j,k,_ηy,e], vgeo[i,j,k,_ηz,e]
+    ζx, ζy, ζz = vgeo[i,j,k,_ζx,e], vgeo[i,j,k,_ζy,e], vgeo[i,j,k,_ζz,e]
+    z = vgeo[i,j,k,_z,e]
+
+    U, V, W = Q[i, j, k, _U, e], Q[i, j, k, _V, e], Q[i, j, k, _W, e]
+    ρ, E = Q[i, j, k, _ρ, e], Q[i, j, k, _E, e]
+
+    P = gdm1*(E - (U^2 + V^2 + W^2)/(2*ρ) - ρ*gravity*z)
+
+    ρinv = 1 / ρ
+
+    fluxρ_x = U
+    fluxU_x = ρinv * U * U + P
+    fluxV_x = ρinv * U * V
+    fluxW_x = ρinv * U * W
+    fluxE_x = ρinv * U * (E + P)
+
+    fluxρ_y = V
+    fluxU_y = ρinv * V * U
+    fluxV_y = ρinv * V * V + P
+    fluxW_y = ρinv * V * W
+    fluxE_y = ρinv * V * (E + P)
+
+    fluxρ_z = W
+    fluxU_z = ρinv * W * U
+    fluxV_z = ρinv * W * V
+    fluxW_z = ρinv * W * W + P
+    fluxE_z = ρinv * W * (E + P)
+
+    s_F[i, j,  _ρ] = MJ * (ξx * fluxρ_x + ξy * fluxρ_y + ξz * fluxρ_z)
+    s_F[i, j,  _U] = MJ * (ξx * fluxU_x + ξy * fluxU_y + ξz * fluxU_z)
+    s_F[i, j,  _V] = MJ * (ξx * fluxV_x + ξy * fluxV_y + ξz * fluxV_z)
+    s_F[i, j,  _W] = MJ * (ξx * fluxW_x + ξy * fluxW_y + ξz * fluxW_z)
+    s_F[i, j,  _E] = MJ * (ξx * fluxE_x + ξy * fluxE_y + ξz * fluxE_z)
+
+    s_G[i, j,  _ρ] = MJ * (ηx * fluxρ_x + ηy * fluxρ_y + ηz * fluxρ_z)
+    s_G[i, j,  _U] = MJ * (ηx * fluxU_x + ηy * fluxU_y + ηz * fluxU_z)
+    s_G[i, j,  _V] = MJ * (ηx * fluxV_x + ηy * fluxV_y + ηz * fluxV_z)
+    s_G[i, j,  _W] = MJ * (ηx * fluxW_x + ηy * fluxW_y + ηz * fluxW_z)
+    s_G[i, j,  _E] = MJ * (ηx * fluxE_x + ηy * fluxE_y + ηz * fluxE_z)
+
+    r_Hρ = MJ * (ζx * fluxρ_x + ζy * fluxρ_y + ζz * fluxρ_z)
+    r_HU = MJ * (ζx * fluxU_x + ζy * fluxU_y + ζz * fluxU_z)
+    r_HV = MJ * (ζx * fluxV_x + ζy * fluxV_y + ζz * fluxV_z)
+    r_HW = MJ * (ζx * fluxW_x + ζy * fluxW_y + ζz * fluxW_z)
+    r_HE = MJ * (ζx * fluxE_x + ζy * fluxE_y + ζz * fluxE_z)
+
+    # one shared access per 10 flops
+    for n = 1:Nq
+      Dkn = s_D[k, n]
+
+      r_rhsρ[n] += Dkn * r_Hρ
+      r_rhsU[n] += Dkn * r_HU
+      r_rhsV[n] += Dkn * r_HV
+      r_rhsW[n] += Dkn * r_HW
+      r_rhsE[n] += Dkn * r_HE
+    end
+
+    r_rhsW[k,i,j] -= MJ * ρ * gravity
+
+    sync_threads()
+    # loop of ξ-grid lines
+    for n = 1:Nq
+      Dni = s_D[n, i]
+      Dnj = s_D[n, j]
+
+      r_rhsρ[k] += Dni * s_F[n, j, _ρ]
+      r_rhsρ[k] += Dnj * s_G[i, n, _ρ]
+
+      r_rhsU[k] += Dni * s_F[n, j, _U]
+      r_rhsU[k] += Dnj * s_G[i, n, _U]
+
+      r_rhsV[k] += Dni * s_F[n, j, _V]
+      r_rhsV[k] += Dnj * s_G[i, n, _V]
+
+      r_rhsW[k] += Dni * s_F[n, j, _W]
+      r_rhsW[k] += Dnj * s_G[i, n, _W]
+
+      r_rhsE[k] += Dni * s_F[n, j, _E]
+      r_rhsE[k] += Dnj * s_G[i, n, _E]
+    end
+  end # k
+
+  for k in 1:Nq
+    MJI = vgeo[i, j, k, _MJI, e]
+
+    rhs[i, j, k, _U, e] += MJI*r_rhsU[k]
+    rhs[i, j, k, _V, e] += MJI*r_rhsV[k]
+    rhs[i, j, k, _W, e] += MJI*r_rhsW[k]
+    rhs[i, j, k, _ρ, e] += MJI*r_rhsρ[k]
+    rhs[i, j, k, _E, e] += MJI*r_rhsE[k]
+  end
+  end
+  nothing
+end
+# }}}
 
 
 function main(nelem, N, DFloat)
@@ -866,6 +1011,21 @@ function main(nelem, N, DFloat)
     @show CUDAdrv.@elapsed @cuda(threads=(N+1, N+1), blocks=nelem, maxregs=255,
           volumerhs_v4!(CUDA(), Val(3), Val(N), Val(nmoist), Val(ntrace),
                         d_rhs_v4, d_Q, d_vgeo, DFloat(grav), d_D, nelem))
+
+    # # The compliation of the following kernel is currently broken
+    # rhs_v5 = zeros(DFloat, Nq, Nq, Nq, nvar, nelem)
+    # d_rhs_v5 = CuArray(rhs_v5)
+
+    # @cuda(threads=(N+1, N+1), blocks=nelem, maxregs=255,
+    #       volumerhs_v5!(Val(3), Val(N), Val(nmoist), Val(ntrace),
+    #                     d_rhs_v5, d_Q, d_vgeo, DFloat(grav), d_D, nelem))
+    # rhs_v5 .= d_rhs_v5
+    # @show norm_v5 = norm(rhs_v5)
+    # @show norm_v1 - norm_v5
+
+    # @show CUDAdrv.@elapsed @cuda(threads=(N+1, N+1), blocks=nelem, maxregs=255,
+    #       volumerhs_v5!(Val(3), Val(N), Val(nmoist), Val(ntrace), d_rhs_v5,
+    #                     d_Q, d_vgeo, DFloat(grav), d_D, nelem))
   end
 
   nothing
